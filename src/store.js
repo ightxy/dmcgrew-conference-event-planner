64f2209c21ee508e414315ea86e6f70e9b85777a
ightxy <dmcgrew723@gmail.com>
2025-01-05 17:45:30 -0500

    added logic to avSlice.js and ConferenceEvent.jsx

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 // store.js
 import { configureStore } from '@reduxjs/toolkit';
 import venueReducer from './venueSlice';
+import avReducer from './venueSlice';
 
 export default configureStore({
   reducer: {
     venue: venueReducer,
+    av: avReducer,
   },
 });
